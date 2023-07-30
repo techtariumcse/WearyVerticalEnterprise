522657fa43051dfc8952eec3d83860e63381e5b8
techtariumcse <138984089+techtariumcse@users.noreply.github.com>
2023-07-30 06:32:03 +0000

    Commit for assignment 6 task set 4 ( add book by using book form and delete book button)

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 function myFunction() {
   alert("Comming Soon !");
 }
-
-
-
 function myFunction1() {
   var name = document.getElementById("bookname").value;
   var author = document.getElementById("author").value;
@@ -14,6 +11,36 @@ function myFunction1() {
   var briefdescription = document.getElementById("briefdescription").value;
   var bookprice = document.getElementById("bookprice").value;
 
-  alert("Name : " + name + "\n Author :" + author + "\n Language : " + language + "\n Available : " + available + "\n Published date : " + publisheddate + "\n Languages : " + Languages + "\n Brief description : " + briefdescription + "\n Price in rupees : " + bookprice);
+  let date1 = new Date(publisheddate).getTime();
+  let date2 = new Date().getTime();
+  
+  if(name === ""){
+    alert("Name cannot be null");
+  }else if(author === ""){
+     alert("Author cannot be null");
+  }else if(date1 > date2){
+    alert("Makes sure that the published date is in the past");
+    
+  }else if(briefdescription.length > 250){
+    alert("Makes sure that the description is less than 250 characters");
+  }else if(bookprice < 0){
+    alert("Book price can't be negative");
+  }
+  else{
+    alert("Name : " + name + "\n Author :" + author + "\n Language : " + language + "\n Available : " + available + "\n Published date : " + publisheddate + "\n Languages : " + Languages + "\n Brief description : " + briefdescription + "\n Price in rupees : " + bookprice);
+  }
+
+  document.getElementById("myText").innerHTML = name;
+   document.getElementById("myText1").innerHTML = author;
+   document.getElementById("myText2").innerHTML = bookprice;
+   document.getElementById("myText3").innerHTML = language;
+}
+
+function myFunction2(){
+   document.getElementById("myText").innerHTML = null;
+   document.getElementById("myText1").innerHTML = null;
+   document.getElementById("myText2").innerHTML = null;
+   document.getElementById("myText3").innerHTML = null;
+  
 }
 
